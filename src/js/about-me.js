3940aa71c7f1df56082102c238f44a75b0148103
Aleksander <hurieiev.al@gmail.com>
2024-07-07 17:15:15 +0300

    work accord

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -3,5 +3,5 @@ import 'accordion-js/dist/accordion.min.css';
 
 export const accordionAboutMe = new Accordion('.accordion-container', {
   duration: 500,
-  // openOnInit: [0],
+  openOnInit: [0],
 });
